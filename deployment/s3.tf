f07478dca0cce00c49ed4f4f60e1fcd45ba1061b
Andrew Schrader <andrew.r.schrader@gmail.com>
2022-07-15 14:29:58 -0500

    Made the lambda deploy bucket non-public

--- deployment/s3.tf
+++ deployment/s3.tf
@@ -6,6 +6,15 @@ resource aws_s3_bucket dist_bucket {
   bucket = "releasethehounds-lambdas-dist"
 }
 
+resource aws_s3_bucket_public_access_block dist_bucket_public_access {
+  bucket = aws_s3_bucket.dist_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource aws_s3_object dist_object {
   key                    = local.dist_file_name
   bucket                 = aws_s3_bucket.dist_bucket.id
